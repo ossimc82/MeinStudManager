ed4e28a2edf3f16c315fd244699ce053db361d42
ossimc82 <speisewagen96@gmx.de>
2022-01-27 14:30:38 +0100

    Change proxy config to allow variable urls at the API endpoint
    This allows you to do something like /api/{controller}/{enpoint}/{var2}/.../{varN}
    Example: /api/user/list/{userId} -> where {userId} would be the guid

--- MeinStudManager/ClientApp/proxy.conf.js
+++ MeinStudManager/ClientApp/proxy.conf.js
@@ -6,8 +6,8 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const PROXY_CONFIG = [
   {
     context: [
-      "/api/weatherforecast",
-   ],
+      "/api/**",
+    ],
     target: target,
     secure: false
   }
